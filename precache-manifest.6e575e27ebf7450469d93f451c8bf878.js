0ef68b51241aff650fbbc1d940cc402e982f8fce
Пользователь <kiselevgleb@gmail.com>
2020-09-22 23:44:51 +0300

    added github pages build

---
+++ precache-manifest.6e575e27ebf7450469d93f451c8bf878.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a1f97a21fc9fa33e80e596ff33d4043",
+    "url": "/react-steps/index.html"
+  },
+  {
+    "revision": "7c8d18c439399e37a493",
+    "url": "/react-steps/static/css/main.e491d6c9.chunk.css"
+  },
+  {
+    "revision": "cc61ef66a781cb3bc104",
+    "url": "/react-steps/static/js/2.38529f7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-steps/static/js/2.38529f7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c8d18c439399e37a493",
+    "url": "/react-steps/static/js/main.fb7a92b1.chunk.js"
+  },
+  {
+    "revision": "12b3d82c9a352e78cdf0",
+    "url": "/react-steps/static/js/runtime-main.8fefd0f1.js"
+  }
+]);
